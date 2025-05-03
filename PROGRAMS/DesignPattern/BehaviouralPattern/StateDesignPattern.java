36a97ac972399db8fc89dd672bbd0b7cc823f1bd
ashutoshsaxena2002 <saxenasiddharth2002@gmail.com>
2025-05-03 10:42:53 +0530

    commit for design pattern

---
+++ PROGRAMS/DesignPattern/BehaviouralPattern/StateDesignPattern.java
@@ -0,0 +1,2 @@
+package com.one97.OBCCPanel.practice.paypal.Graphs.PROGRAMS.DesignPattern.BehaviouralPattern;public class StateDesignPattern {
+}
---
+++ PROGRAMS/DesignPattern/StructuralPattern/ProxyDesignPattern.java
@@ -0,0 +1,2 @@
+package com.one97.OBCCPanel.practice.paypal.Graphs.PROGRAMS.DesignPattern.StructuralPattern;public class ProxyDesignPattern {
+}
